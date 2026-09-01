cca0929ebca4214fd66c72cbaae405361e39df3a
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2026-09-01 22:28:26 +0600

    Update datatype_summary.js

--- JAVASCRIPT/datatype_summary.js
+++ JAVASCRIPT/datatype_summary.js
@@ -36,3 +36,23 @@ console.log(typeof myFunction)//this will return 'function'. it is a special typ
 //array is also a special type of object. it is an object type but it is used to store multiple values in a single variable.
 //null is also a special type of object. it is an object type but it is used to represent the absence of any value.
 //anotherId will return 'symbol'. it is a unique identifier. it is a symbol type.
+
+//*********************memory management******************
+
+//stack memory -> it is used to store primitive data types. it is a fixed size memory. it is faster than heap memory. it is used for static memory allocation. it is used for storing variables and function calls. it is used for storing local variables. it is used for storing return values.
+//heap memory -> it is used to store non-primitive data types. it is a dynamic size memory. it is slower than stack memory. it is used for dynamic memory allocation. it is used for storing objects and arrays. it is used for storing global variables. it is used for storing function objects.
+//from stack memory we get the copy of the primitive data types and from heap memory we get the reference of the non-primitive data types. so if we change the value of the primitive data type then it will not affect the original value but if we change the value of the non-primitive data type then it will affect the original value because we are changing the reference of the object or array.
+
+let myName = 'Eva'//stack memory
+let anotherName = myName//stack memory. here anotherName is a copy of myName. so if we change the value of anotherName then it will not affect the value of myName.
+anotherName = 'Alice'
+
+console.log(myName)//Eva
+console.log(anotherName)//Alice
+
+let userOne = { name: 'Eva' }//heap memory
+let userTwo = userOne//heap memory. here userTwo is a reference of userOne. so if we change the value of userTwo then it will affect the value of userOne.
+userTwo.name = 'Alice'
+
+console.log(userOne.name)//Alice
+console.log(userTwo.name)//Alice
